56320e24b85b0ec7f6999f061f957d1921e2a1b0
Vov <aderbsinc.2021@gmail.com>
2022-04-17 12:07:20 +0300

    dsfdsf

--- js/jh.js
+++ js/jh.js
@@ -1,3 +1,13 @@
 function add_or_del () {
-    
+    var us_i = {
+        us_i1: {
+            usi1_nm:0,
+            usi1_sr:0,
+            usi1_ag:0
+        }
+    };
+    Object.defineProperty(us_i, "us_i2", {value:0, writable:true, enumerable:true, configurable:true});
+    if(us_i.hasOwnProperty("us_i2") == true && us_i.us_i1.usi1_nm instanceof Number == true) {
+        
+    }
 }
